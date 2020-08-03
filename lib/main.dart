e2e53ba63fea3d674effdbab91d5f399a98d8d74
Tim Eitel <timeitel@outlook.com>
2020-08-03 17:57:05 +0800

    style: Theme background color

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primaryColor: Color(0xFF3EBACE),
           accentColor: Color(0xFFD8ECF1),
-          scaffoldBackgroundColor: Color(0xFFD8ECF1)),
+          scaffoldBackgroundColor: Color(0xFFF3F5F7)),
       home: HomeScreen(),
     );
   }
