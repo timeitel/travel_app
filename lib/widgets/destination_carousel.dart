523af35135f5caf5d96b5556ccc87ea4ba4a69cb
Tim Eitel <timeitel@outlook.com>
2020-08-03 21:29:09 +0800

    add: destinations widget header

---
+++ lib/widgets/destination_carousel.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class DestinationCarousel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Top Destinations',
+                style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5),
+              ),
+              GestureDetector(
+                onTap: () => print('See All'),
+                child: Text(
+                  'See all',
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.0),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
