fc0ad813b77994447907ac52f0c6fb76f331f4d3
Tim Eitel <timeitel@outlook.com>
2020-08-03 21:29:09 +0800

    add: Destinations widget header

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:travel_app/widgets/destination_carousel.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -64,6 +65,10 @@ class _HomeScreenState extends State<HomeScreen> {
                 .map((MapEntry map) => _buildIcon(map.key))
                 .toList(),
           ),
+          SizedBox(
+            height: 20.0,
+          ),
+          DestinationCarousel(),
         ],
       )),
     );
