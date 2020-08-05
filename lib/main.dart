a9caf2ed8225b16b33aab1e1a94ec24b611c8ab1
Tim Eitel <timeitel@outlook.com>
2020-08-05 22:46:52 +0800

    feat: Login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:travel_app/screens/home_screen.dart';
+import 'package:travel_app/screens/login_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
           primaryColor: Color(0xFF3EBACE),
           accentColor: Color(0xFFD8ECF1),
           scaffoldBackgroundColor: Color(0xFFF3F5F7)),
-      home: HomeScreen(),
+      home: LoginScreen(),
     );
   }
 }
