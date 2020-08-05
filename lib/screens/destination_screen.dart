1732adcbb0b5de93b8747cdcf88a43d7a2fcdbf8
Tim Eitel <timeitel@outlook.com>
2020-08-05 19:13:25 +0800

    add: Destination screen hero

--- lib/screens/destination_screen.dart
+++ lib/screens/destination_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:travel_app/models/destination_model.dart';
 
 class DestinationScreen extends StatefulWidget {
@@ -13,6 +14,110 @@ class DestinationScreen extends StatefulWidget {
 class _DestinationScreenState extends State<DestinationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30.0),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black26,
+                            offset: Offset(0.0, 2.0),
+                            blurRadius: 6.0)
+                      ]),
+                  child: Hero(
+                    tag: widget.destination.imageUrl,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40),
+                          bottomRight: Radius.circular(40)),
+                      child: Image(
+                        image: AssetImage(widget.destination.imageUrl),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(
+                          icon: Icon(Icons.arrow_back),
+                          iconSize: 30.0,
+                          color: Colors.black,
+                          onPressed: () => Navigator.pop(context)),
+                      Row(
+                        children: <Widget>[
+                          IconButton(
+                              icon: Icon(Icons.search),
+                              iconSize: 30.0,
+                              color: Colors.black,
+                              onPressed: () => Navigator.pop(context)),
+                          IconButton(
+                              icon: Icon(FontAwesomeIcons.sortAmountDown),
+                              iconSize: 23.0,
+                              color: Colors.black,
+                              onPressed: () => Navigator.pop(context)),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  left: 20.0,
+                  bottom: 20.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        widget.destination.city,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 35.0,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 1.2),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            FontAwesomeIcons.locationArrow,
+                            size: 15.0,
+                            color: Colors.white60,
+                          ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Text(
+                            widget.destination.country,
+                            style: TextStyle(
+                                color: Colors.white70, fontSize: 20.0),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  right: 20.0,
+                  bottom: 20.0,
+                  child: Icon(
+                    Icons.location_on,
+                    color: Colors.white70,
+                    size: 25.0,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
