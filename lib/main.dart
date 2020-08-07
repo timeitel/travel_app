69adcc38516e2b78f7b9027009b18b0a6959d031
Tim Eitel <timeitel@outlook.com>
2020-08-07 17:18:05 +0800

    add: authentication wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:travel_app/screens/login_screen.dart';
+import 'package:travel_app/screens/wrapper.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -15,7 +13,7 @@ class MyApp extends StatelessWidget {
           primaryColor: Color(0xFF3EBACE),
           accentColor: Color(0xFFD8ECF1),
           scaffoldBackgroundColor: Color(0xFFF3F5F7)),
-      home: LoginScreen(),
+      home: Wrapper(),
     );
   }
 }
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/screens/login_screen.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return home or login screen
+    return LoginScreen();
+  }
+}
