5739b4b0f70b89be2e926fee0f243f1f6bbd3c04
Tim Eitel <timeitel@outlook.com>
2020-08-10 23:27:20 +0800

    refactor: update constants library

--- lib/screens/authenticate/login_screen.dart
+++ lib/screens/authenticate/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:travel_app/services/auth_service.dart';
-import 'package:travel_app/widgets/constants.dart';
+import 'package:travel_app/widgets/constants.dart' as Constants;
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -20,23 +20,7 @@ class _LoginScreenState extends State<LoginScreen> {
           onTap: () => FocusScope.of(context).unfocus(),
           child: Stack(
             children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color(0xFF73AEF5),
-                      Color(0xFF61A4F1),
-                      Color(0xFF478DE0),
-                      Color(0xFF398AE5),
-                    ],
-                    stops: [0.1, 0.4, 0.7, 0.9],
-                  ),
-                ),
-              ),
+              Constants.gradientContainer,
               Container(
                 height: double.infinity,
                 child: SingleChildScrollView(
@@ -45,125 +29,127 @@ class _LoginScreenState extends State<LoginScreen> {
                     horizontal: 40.0,
                     vertical: 120.0,
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      _buildLoginHero(),
-                      SizedBox(height: 30.0),
-                      Text(
-                        'Email',
-                        style: kLabelStyle,
-                      ),
-                      SizedBox(height: 10.0),
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        decoration: kBoxDecorationStyle,
-                        height: 60.0,
-                        child: TextField(
-                          keyboardType: TextInputType.emailAddress,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'OpenSans',
-                          ),
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.only(top: 14.0),
-                            prefixIcon: Icon(
-                              Icons.email,
-                              color: Colors.white,
-                            ),
-                            hintText: 'Enter your email',
-                            hintStyle: kHintTextStyle,
-                          ),
+                  child: Form(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        _buildLoginHero(),
+                        SizedBox(height: 30.0),
+                        Text(
+                          'Email',
+                          style: Constants.textLabel,
                         ),
-                      ),
-                      SizedBox(height: 30.0),
-                      Text(
-                        'Password',
-                        style: kLabelStyle,
-                      ),
-                      SizedBox(height: 10.0),
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        decoration: kBoxDecorationStyle,
-                        height: 60.0,
-                        child: TextField(
-                          obscureText: true,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'OpenSans',
-                          ),
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.only(top: 14.0),
-                            prefixIcon: Icon(
-                              Icons.lock,
-                              color: Colors.white,
-                            ),
-                            hintText: 'Enter your password',
-                            hintStyle: kHintTextStyle,
-                          ),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          FlatButton(
-                            onPressed: () async {
-                              dynamic result = await _auth.signInAnon();
-                              if (result == null) {
-                                print('error signing in');
-                                print(result);
-                              } else {
-                                print('signedin');
-                                print(result.uid);
-                              }
-                            },
-                            padding: EdgeInsets.only(right: 0.0),
-                            child: Text(
-                              'Continue as guest',
-                              style: kLabelStyle,
-                            ),
-                          ),
-                          FlatButton(
-                            onPressed: () =>
-                                print('Forgot Password Button Pressed'),
-                            padding: EdgeInsets.only(right: 0.0),
-                            child: Text(
-                              'Forgot password?',
-                              style: kLabelStyle,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        padding: EdgeInsets.symmetric(vertical: 25.0),
-                        width: double.infinity,
-                        child: RaisedButton(
-                          elevation: 5.0,
-                          onPressed: () {
-                            print('Logging in ');
-                          },
-                          padding: EdgeInsets.all(15.0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                          ),
-                          color: Colors.white,
-                          child: Text(
-                            'LOGIN',
+                        SizedBox(height: 10.0),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          decoration: Constants.decorationStyle,
+                          height: 60.0,
+                          child: TextFormField(
+                            keyboardType: TextInputType.emailAddress,
                             style: TextStyle(
-                              color: Color(0xFF527DAA),
-                              letterSpacing: 1.5,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
                               fontFamily: 'OpenSans',
                             ),
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.only(top: 14.0),
+                              prefixIcon: Icon(
+                                Icons.email,
+                                color: Colors.white,
+                              ),
+                              hintText: 'Enter your email',
+                              hintStyle: Constants.textHint,
+                            ),
                           ),
                         ),
-                      ),
-                      _buildSignupBtn(),
-                    ],
+                        SizedBox(height: 30.0),
+                        Text(
+                          'Password',
+                          style: Constants.textLabel,
+                        ),
+                        SizedBox(height: 10.0),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          decoration: Constants.decorationStyle,
+                          height: 60.0,
+                          child: TextField(
+                            obscureText: true,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'OpenSans',
+                            ),
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.only(top: 14.0),
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: Colors.white,
+                              ),
+                              hintText: 'Enter your password',
+                              hintStyle: Constants.textHint,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            FlatButton(
+                              onPressed: () async {
+                                dynamic result = await _auth.signInAnon();
+                                if (result == null) {
+                                  print('error signing in');
+                                  print(result);
+                                } else {
+                                  print('signedin');
+                                  print(result.uid);
+                                }
+                              },
+                              padding: EdgeInsets.only(right: 0.0),
+                              child: Text(
+                                'Continue as guest',
+                                style: Constants.textLabel,
+                              ),
+                            ),
+                            FlatButton(
+                              onPressed: () =>
+                                  print('Forgot Password Button Pressed'),
+                              padding: EdgeInsets.only(right: 0.0),
+                              child: Text(
+                                'Forgot password?',
+                                style: Constants.textLabel,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 25.0),
+                          width: double.infinity,
+                          child: RaisedButton(
+                            elevation: 5.0,
+                            onPressed: () {
+                              print('Logging in ');
+                            },
+                            padding: EdgeInsets.all(15.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                            color: Colors.white,
+                            child: Text(
+                              'LOGIN',
+                              style: TextStyle(
+                                color: Color(0xFF527DAA),
+                                letterSpacing: 1.5,
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'OpenSans',
+                              ),
+                            ),
+                          ),
+                        ),
+                        _buildSignupBtn(),
+                      ],
+                    ),
                   ),
                 ),
               ),
--- lib/widgets/constants.dart
+++ lib/widgets/constants.dart
@@ -1,17 +1,19 @@
+library constants;
+
 import 'package:flutter/material.dart';
 
-final kHintTextStyle = TextStyle(
+final textHint = TextStyle(
   color: Colors.white54,
   fontFamily: 'OpenSans',
 );
 
-final kLabelStyle = TextStyle(
+final textLabel = TextStyle(
   color: Colors.white,
   fontWeight: FontWeight.bold,
   fontFamily: 'OpenSans',
 );
 
-final kBoxDecorationStyle = BoxDecoration(
+final decorationStyle = BoxDecoration(
   color: Color(0xFF6CA8F1),
   borderRadius: BorderRadius.circular(10.0),
   boxShadow: [
@@ -21,4 +23,22 @@ final kBoxDecorationStyle = BoxDecoration(
       offset: Offset(0, 2),
     ),
   ],
+);
+
+final gradientContainer = Container(
+  height: double.infinity,
+  width: double.infinity,
+  decoration: BoxDecoration(
+    gradient: LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [
+        Color(0xFF73AEF5),
+        Color(0xFF61A4F1),
+        Color(0xFF478DE0),
+        Color(0xFF398AE5),
+      ],
+      stops: [0.1, 0.4, 0.7, 0.9],
+    ),
+  ),
 );
